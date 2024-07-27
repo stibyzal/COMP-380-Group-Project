cbf48f1c3cc794e3102162ee5170f056e054d441
stibyzal <169078060+stibyzal@users.noreply.github.com>
2024-07-26 21:16:44 -0700

    Add files via upload

--- MainWindowController.java
+++ MainWindowController.java
@@ -1,6 +1,11 @@
 package com.example.projcomp380;
 
+import com.sun.tools.javac.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -8,6 +13,7 @@ import java.time.LocalDate;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class MainWindowController {
 
@@ -55,43 +61,47 @@ public class MainWindowController {
         alert.setContentText(message);
         alert.showAndWait();
 
-}
+    }
 
     @FXML  //  method search parameters
-    protected void searchRoomAvailability() {
+    protected void searchRoomAvailability(ActionEvent actionEvent) throws Exception {
 
         // get input values from customer
         LocalDate checkInDate = checkInDatePicker.getValue();
         LocalDate checkOutDate = checkOutDatePicker.getValue();
         String numOfGuestsText = numOfGuestsField.getText();
 
-            // check if all dates are enteredd
-            if (checkInDate == null || checkOutDate == null) {
-                displayErrorBox(AlertType.ERROR, "Date Error", "Please select check-in and check-out dates.");
-                return;
+        // check if all dates are entered
+        if (checkInDate == null || checkOutDate == null) {
+            displayErrorBox(AlertType.ERROR, "Date Error", "Please select check-in and check-out dates.");
+            return;
         }
 
-            // check if correct input for guests
-            if (numOfGuestsText == null || numOfGuestsText.trim().isEmpty()) {
-                displayErrorBox(AlertType.ERROR, "Guest Error", "Please enter the number of guests.");
-                return;
+        // check if correct input for guests
+        if (numOfGuestsText == null || numOfGuestsText.trim().isEmpty()) {
+            displayErrorBox(AlertType.ERROR, "Guest Error", "Please enter the number of guests.");
+            return;
         }
 
-            // check if dates entered are correct
-            if (checkInDate.isAfter(checkOutDate)) {
-                displayErrorBox(AlertType.ERROR, "Date Error", "Check-in date cannot be after check-out date.");
-                return;
+        // check if dates entered are correct
+        if (checkInDate.isAfter(checkOutDate)) {
+            displayErrorBox(AlertType.ERROR, "Date Error", "Check-in date cannot be after check-out date.");
+            return;
         }
 
-            // placeholder, working on second window
-             System.out.println("tbd second window");
+        // placeholder, working on second window
+        FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("second-window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
 
-            // to do
-            // read file - alert window for no rooms
-            // read file - new scene for available rooms
+        // to do
+        // read file - alert window for no rooms
+        // read file - new scene for available rooms
 
-            // if room is available -> next window
-            // if room is NOT available -> another alert box
+        // if room is available -> next window
+        // if room is NOT available -> another alert box
 
     }
 }
---
+++ SecondWindowController.java
@@ -0,0 +1,16 @@
+package com.example.projcomp380;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import java.time.LocalDate;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+
+
+public class SecondWindowController {
+
+}
