1a59a6b25ff022cb65e672f8d4cd23121b517c9f
Richwhat28 <richwhat1995@gmail.com>
2024-07-29 15:29:08 -0700

    Add files via upload

---
+++ SecondWindow.java
@@ -0,0 +1,35 @@
+package com.example.hotelcalifornia;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SecondWindow extends Application {
+
+    // Main Window
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("second-window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 816, 360);
+        stage.setTitle("Hotel California");
+        stage.setX(100);
+        stage.setY(100);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
