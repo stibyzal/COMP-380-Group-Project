227524a7f19428fad506fb40c9920b683d28c805
stibyzal <169078060+stibyzal@users.noreply.github.com>
2024-07-30 17:13:26 -0700

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,15 +1,15 @@
-package javaOOP.HotelCalifornia;
+package com.example.projcomp380;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Payment {
     private String cardNumber;
-    private String expirationDate;
+    private String expirationMonth;
+    private String expirationYear;
     private String cvvNumber;
-   Address address = new Address();
-    
+    //Address address = new Address();
+
     public Payment(){}  //Default constructor
-    public Payment(String cardNumber, String expirationDate, String cvvNumber){
+    public Payment(String cardNumber, String expirationMonth, String expirationYear, String cvvNumber){
 
     }
 
@@ -17,8 +17,11 @@ public class Payment {
     public void setCardNumber(String cardNumber){
         this.cardNumber = cardNumber;
     }
-    public void setExpirationDate(String expirationDate){
-        this.expirationDate = expirationDate;
+    public void setExpirationYear(String expirationYear){
+        this.expirationYear = expirationYear;
+    }
+    public void setExpirationMonth(String expirationMonth){
+        this.expirationMonth = expirationMonth;
     }
     public void setCvvNumber(String cvvNumber){
         this.cvvNumber = cvvNumber;
@@ -28,8 +31,11 @@ public class Payment {
     public String getCardNumber(){
         return this.cardNumber;
     }
-    public String getCardExpirationDate(){
-        return this.expirationDate;
+    public String getExpirationMonth(){
+        return this.expirationMonth;
+    }
+    public String getExpirationYear(){
+        return this.expirationYear;
     }
     public String getCvvNumber(){
         return this.cvvNumber;
@@ -41,17 +47,17 @@ public class Payment {
         System.out.print("Enter card number: ");
         this.cardNumber = scanner.nextLine();
         System.out.println();
-        System.out.print("Enter card expiration date: ");
-        this.expirationDate = scanner.nextLine();
+        System.out.print("Enter card expiration month: ");
+        this.expirationMonth = scanner.nextLine();
+        System.out.print("Enter card expiration year: ");
+        this.expirationYear = scanner.nextLine();
         System.out.println();
         System.out.print("Enter the ccv number: ");
         this.cvvNumber = scanner.nextLine();
         System.out.println();
         System.out.println("Billing address: ");
-        address.getAddress();  // Calling getAddress method in address 
+        //address.getAddress();  // Calling getAddress method in address
 
     }
-
-
 
 }
