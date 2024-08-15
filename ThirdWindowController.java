da3577716ee2d8f959bdd30afc89ffc7e61be691
stibyzal <169078060+stibyzal@users.noreply.github.com>
2024-08-14 19:21:41 -0700

    Update ThirdWindowController.java

--- ThirdWindowController.java
+++ ThirdWindowController.java
@@ -15,71 +15,207 @@ import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+ * Third Window Controller
+ * Aug 8, 2024
+ * @author K. Dela Merced, B. Ascorra
+ * The third window that shows the user the details of their booking.
+ * The user inputs their payment method details on this window.
+ */
 public class ThirdWindowController {
-
+    /**
+     * Image of the room that was selected by user.
+     */
     @FXML
     private ImageView roomImageView;
+
+    /**
+     * Label on the window to show the user's check in date.
+     */
     @FXML
     private Label checkInDateLabel;
+
+    /**
+     * Label on the window to show the user's check out date.
+     */
     @FXML
     private Label checkOutDateLabel;
+
+    /**
+     * Label on the window to show the number of guests for the reservation.
+     */
     @FXML
     private Label guestsLabel;
+
+    /**
+     * Label on the window to show the user's type of room selected.
+     */
     @FXML
     private Label roomTypeLabel;
+
+    /**
+     * Label on the window to show the description of the room selected.
+     */
     @FXML
     private Label roomDescriptionLabel;
+
+    /**
+     * Label on the window to show the price per night for reserving the selected.
+     */
     @FXML
     private Label priceLabel;
+
+    /**
+     * Label on the window to show the total price of the reservation.
+     */
     @FXML
     private Label totalPriceLabel;
 
+    /**
+     * Text field in the window where the user inputs their address.
+     */
     @FXML
     private TextField addressTextField;
+
+    /**
+     * Text field in the window where the user inputs their city.
+     */
     @FXML
     private TextField cityTextField;
+
+    /**
+     * Text field in the window where the user inputs their zipcode.
+     */
     @FXML
     private TextField zipTextField;
+
+    /**
+     * Text field in the window where the user inputs their country.
+     */
     @FXML
     private TextField countryTextField;
+
+    /**
+     * Text field in the window where the user inputs their state.
+     */
     @FXML
     private TextField stateTextField;
+
+    /**
+     * Text field in the window where the user inputs their email.
+     */
     @FXML
     private TextField emailText;
-    @FXML
-    private TextField billAddText;
+
+    /**
+     * Text field in the window where the user inputs their cvv.
+     */
     @FXML
     private TextField cvvText;
+
+    /**
+     * Text field in the window where the user inputs their card expiration year.
+     */
     @FXML
     private TextField expYearText;
+
+    /**
+     * Text field in the window where the user inputs their card expiration month.
+     */
     @FXML
     private TextField expMonthText;
+
+    /**
+     * Text field in the window where the user inputs their card number.
+     */
     @FXML
     private TextField cardNumText;
+
+    /**
+     * Text field in the window where the user inputs their first name.
+     */
     @FXML
     private TextField firstNameText;
+
+    /**
+     * Text field in the window where the user inputs their last name.
+     */
     @FXML
     private TextField lastNameText;
+
+    /**
+     * Text field in the window where the user inputs their phone number.
+     */
     @FXML
     private TextField phoneNumText;
-    @FXML
-    private Text nightsLabel;
+
+    /**
+     * Button on the window that cancels the reservation process.
+     */
     @FXML
     private Button cancelButton;
+
+    /**
+     * Button on the window that confirms the reservation.
+     */
     @FXML
     private Button confirmButton;
 
+
     // attributes to store reservation details
+    /**
+     * Represents the user's check in date.
+     */
     private LocalDate checkInDate;
+
+    /**
+     * Represents the user's check out date.
+     */
     private LocalDate checkOutDate;
+
+    /**
+     * Represents the user's number of guests for the reservation.
+     */
     private int numGuests;
+
+    /**
+     * Represents the user's room type that was selected.
+     */
     private String roomType;
+
+    /**
+     * Represents the price per night based on the room type selected.
+     */
     private double price;
+
+    /**
+     * Represents the total price of the entire reservation.
+     */
     private double totalPrice;
+
+    /**
+     * Represents the description of the room based on the type selected.
+     */
     private String roomDescription;
+
+    /**
+     * Represents the image of the room based on the type selected.
+     */
     private Image roomImage;
 
     // method to get reservation customer input from second window
+
+    /**
+     * Gets the user's reservation details from the previous window.
+     * It then displays the details on the current window.
+     * @param checkInDate the check-in date
+     * @param checkOutDate the check-out date
+     * @param numGuests the number of guests
+     * @param roomType the type of room
+     * @param price the price per night
+     * @param roomDescription the description of the room
+     * @param image the image of the room
+     */
     public void getReservationDetails(LocalDate checkInDate, LocalDate checkOutDate, int numGuests, String roomType, double price, String roomDescription, Image image) {
 
         // store details in instance variable
@@ -114,14 +250,17 @@ public class ThirdWindowController {
             System.err.println("Image not found for room type: " + roomType);
         }
 
-
         priceLabel.setText(String.valueOf(price)); // display the price per night
         totalPriceLabel.setText(String.valueOf(totalPrice)); // display the total price
-
     }
 
 
     // method to handle reservation after being confirmed by customer
+
+    /**
+     * Gets in all the user input from the text fields and stores them into their respective objects.
+     * Those objects and all their data are then put into text files based on object type.
+     */
     @FXML
     private void confirmReservation() {
         // generate reservation num
@@ -165,6 +304,16 @@ public class ThirdWindowController {
     }
 
     // method to display next window with final summary of reservation
+
+    /**
+     * Sends all the information stored from this window into the next window.
+     * @param customer the customer information
+     * @param payment the payment information
+     * @param address the address information
+     * @param reservation the reservation information
+     * @param reservationNumber the reservation number
+     * @param totalPaid the total cost of the reservation
+     */
     private void showConfirmationWindow(Customer customer, Payment payment, Address address, Reservation reservation, String reservationNumber, double totalPaid) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("fourth-window.fxml"));
@@ -179,7 +328,7 @@ public class ThirdWindowController {
             // Get current stage and close it
             Stage currentStage = (Stage) confirmButton.getScene().getWindow();
             currentStage.close();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
